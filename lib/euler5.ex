2fa68c11e1fc6ebad641ea91461c14fa9d81e47e
Stefc <stefan.boether@gmail.com>
2025-01-26 21:34:09 +0100

    Problem #5 gelöst

--- lib/euler5.ex
+++ lib/euler5.ex
@@ -4,6 +4,7 @@ defmodule Euler5 do
 
     What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20 ?
   """
+  defp primes, do: [2,3,5,7,11,13,17,19]
 
   def  max_aggregate(a,b),
     do: Enum.zip(a,b)
@@ -16,24 +17,38 @@ defmodule Euler5 do
 
   def count_div(_, _, accu), do: accu
 
+  defp prime_factors(n) do
+    Enum.reduce(primes(), [],
+      fn current, accu ->
+        if rem(n,current) == 0 do
+          accu ++ [count_div(n, current)]
+        else
+          accu ++ [0]
+        end
+      end)
+  end
+
   def calc(n) do
-    range = 2..n-1
-    primes = [2,3,5,7,11,13,17,19]
-    prime_factors = Enum.reduce(primes, [], fn (accu, current) -> if(rem(n,current) == 0, count_div(n, current), 0)
+    2..n-1
+    |> Enum.map(&prime_factors/1)
+    |> Enum.reduce(fn cur, acc -> max_aggregate(cur, acc) end)
+    |> Enum.zip(primes())
+    |> Enum.map(fn {e,p} -> trunc(:math.pow(p,e)) end)
+    |> Enum.reduce(1, fn a,b -> a * b end)
   end
 
   # ------------------------------------ Heiko ---------------
 
-  # def solve() do
-  #   Stream.iterate(2 * 3 * 5 * 7 * 9 * 11 * 13 * 17 * 19, fn n -> n + 1 end)
-  #   |> Stream.filter(&dividable?/1)
-  #   |> Enum.take(1)
-  #   |> hd()
-  # end
+  def solve() do
+    Stream.iterate(2 * 3 * 5 * 7 * 9 * 11 * 13 * 17 * 19, fn n -> n + 1 end)
+    |> Stream.filter(&dividable?/1)
+    |> Enum.take(1)
+    |> hd()
+  end
 
-  # defp dividable?(n) do
-  #   Enum.all?(2..21, fn x ->
-  #     rem(n, x) == 0
-  #   end)
-  # end
+  defp dividable?(n) do
+    Enum.all?(2..21, fn x ->
+      rem(n, x) == 0
+    end)
+  end
 end
--- test/euler5_test.exs
+++ test/euler5_test.exs
@@ -1,10 +1,13 @@
 defmodule Euler5Test do
   use ExUnit.Case
 
+  test "count_div",
+    do: assert Euler5.count_div(10, 2) == 5
+
   test "sample",
     do: assert Euler5.calc(10) == 2520
 
   test "problem",
-    do: assert Euler5.calc(20) == 1111
+    do: assert Euler5.calc(20) == 232792560
 
 end
